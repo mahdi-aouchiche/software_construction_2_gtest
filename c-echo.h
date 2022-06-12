d87f9dd676aa2dc1fb4e8eaa1ff4d249d08bfb9d
mahdi-aouchiche <mahdiaouchiche2022@gmail.com>
2022-06-12 12:51:48 -0700

    cleaner code

--- c-echo.h
+++ c-echo.h
@@ -3,12 +3,12 @@
 std::string echo(int length, char** chars)
 { 
 	std::string ret = "";
-       	for(int i = 1; i < length; i++) {
+    for(int i = 1; i < length; i++) {
 		ret += chars[i];
-	       	if(i < length - 1) { 
+	   	if(i < length - 1) { 
 			ret += " ";
-	       	}
-       	}
-       	//ret += "\n";
-       	return ret;
+	   	}
+    }
+    //ret += "\n";
+    return ret;
 }
