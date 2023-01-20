78400f82416544c8f452095b95c84882add8c0a8
mahdi-aouchiche <mahdiaouchiche2022@gmail.com>
2023-01-20 10:15:29 -0800

    Fixed warning: ISO C++ forbids converting a string constant to ‘char*’.

--- src/test.cpp
+++ src/test.cpp
@@ -4,43 +4,43 @@
 using namespace std;
 
 TEST(EchoTest, HelloWorld) {
-       char* test_val[3];
-       test_val[0] = "../c-echo";
-       test_val[1] = "Hello";
-       test_val[2] = "World";
-       EXPECT_EQ("Hello World", echo(3,test_val));
+    char* test_val[3];
+    test_val[0] = (char*)"../c-echo";
+    test_val[1] = (char*)"Hello";
+    test_val[2] = (char*)"World";
+    EXPECT_EQ("Hello World", echo(3,test_val));
 }
 
 TEST(EchoTest, UpperCase) {
-       char* test_val[3];
-       test_val[0] = "../c-echo";
-       test_val[1] = "HELLO";
-       test_val[2] = "WORLD";
-       EXPECT_EQ("HELLO WORLD", echo(3,test_val));
+    char* test_val[3];
+    test_val[0] = (char*)"../c-echo";
+    test_val[1] = (char*)"HELLO";
+    test_val[2] = (char*)"WORLD";
+    EXPECT_EQ("HELLO WORLD", echo(3,test_val));
 }
 
 TEST(EchoTest, SpecialCharacter) {
     char* test_val[2];
-    test_val[0] = "../c-echo";
-    test_val[1] = "!@#";
+    test_val[0] = (char*)"../c-echo";
+    test_val[1] = (char*)"!@#";
     EXPECT_EQ("!@#", echo(2,test_val));
 }
 
 TEST(EchoTest, EmptyString) { 
-       char* test_val[1];
-       test_val[0] = "../c-echo";
-       EXPECT_EQ("", echo(1,test_val));
+    char* test_val[1];
+    test_val[0] = (char*)"../c-echo";
+    EXPECT_EQ("", echo(1,test_val));
 }
 
 TEST(EchoTest, AlphaNumeric) {
-       char* test_val[2]; 
-       test_val[0] = "../c-echo";
-       test_val[1] = "0123";
-       EXPECT_EQ("0123", echo(2,test_val));
+    char* test_val[2]; 
+    test_val[0] = (char*)"../c-echo";
+    test_val[1] = (char*)"0123";
+    EXPECT_EQ("0123", echo(2,test_val));
 } 
 
 int main(int argc, char **argv)
 {
-       ::testing::InitGoogleTest(&argc, argv);
-       return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
